ab8ceed729c9798b592b1824338fd61dd6fe3d49
Mateus Guedes <mxteusgued@gmail.com>
2023-07-12 23:58:41 -0300

    final project

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,11 @@
 import { OrbitControls } from "@react-three/drei";
+import FadingImageDisplacement from "./FadingImageDisplacement";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
-      </mesh>
+      <FadingImageDisplacement />
     </>
   );
 };
